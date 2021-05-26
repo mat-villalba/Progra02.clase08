12560853304f4afe97463f63eeee43c84d08d0f5
mat-villalba <matvillalba@alumno.unlam.edu.ar>
2021-05-26 10:30:23 -0300

    getter y setter generados

--- src/ar/edu/unlam/pb2/eva03/Batalla.java
+++ src/ar/edu/unlam/pb2/eva03/Batalla.java
@@ -10,5 +10,24 @@ public class Batalla {
 	private TipoDeBatalla tipo;
 	private String nombre;
 	private List<Vehiculo> vehiculosEnLaBatalla;
+	public Double getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(Double latitud) {
+		this.latitud = latitud;
+	}
+	public Double getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(Double longitud) {
+		this.longitud = longitud;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 	
 }
